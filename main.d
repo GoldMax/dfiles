23b3e0d37616f9e46bf16a54dda8b2fecd1d2ac5
GoldMax <goldmax3000@gmail.com>
2020-06-19 13:09:37 +0700

    v 0.1

--- main.d
+++ main.d
@@ -1,22 +1,48 @@
 import std.stdio;
+import std.file;
+import std.path;
+import std.algorithm : filter, endsWith;
 
 int main(string[] args)
 {
  try
 	{
+  void printHelp()
+		{
+			writeln("dfiles tools v0.1 Copyright (C) 2020 by Max Goldaev");
+   writeln("Usage: dfiles <dirName>");
+		}
+
 		if(args.length != 2)
 		{
-   writeln("Usage: dfiles dirName");
+		 printHelp();
 			return 1;
 		}
 
+		string dname = args[1];
+		if(dname == "--help" || dname == "-h")
+		{
+			printHelp();
+			return 0;
+		}
 
- writeln(args);
+   if(exists(dname) == false)
+		{
+   writefln("Error!!! Directory [%s] not exists!", dname);
+			return 2;
+		}
+		if(isDir(dname) == false)
+		{
+   writefln("Error!!! Directory [%s] not a directory!", dname);
+			return 2;
+		}
+
+  auto iter = dirEntries(dname, SpanMode.depth,true).filter!(f => f.name.endsWith(".d"));
+  foreach(DirEntry de; iter)
+		 if(de.isFile)
+			 stdout.write(relativePath(de.name, dname), " ");
 
   return 0;
-
-
-
 	}
 	catch(Throwable err)
 	{
